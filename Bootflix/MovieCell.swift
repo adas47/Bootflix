87dae37a780c0c7e5c74e0671ccc346d7e744d88
adas47 <92237683+adas47@users.noreply.github.com>
2022-02-06 17:07:12 -0500

    added .gitignore .gitattributes

---
+++ Bootflix/MovieCell.swift
@@ -0,0 +1,29 @@
+ //
+//  MovieCell.swift
+//  Bootflix
+//
+//  Created by Anshuman Das on 2/5/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Bootflix/MoviesViewController.swift
@@ -0,0 +1,89 @@
+//
+//  MoviesViewController.swift
+//  Bootflix
+//
+//  Created by Anshuman Das on 2/5/22.
+//
+
+import UIKit
+import AlamofireImage
+
+class MoviesViewController: UIViewController, UITableViewDataSource,  UITableViewDelegate {
+   
+    @IBOutlet var tableView: UITableView!
+    var movies = [[String:Any]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // Do any additional setup after loading the view.
+        
+        print ("Hello")
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { [self] (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+                 self.movies = dataDictionary["results"] as! [[String:Any]]
+                 self.tableView.reloadData()
+                 
+                 print(dataDictionary)
+                 
+                    // TODO: Get the array of movies
+                    // TODO: Store the movies in a property to use elsewhere
+                    // TODO: Reload your table view data
+
+             }
+        }
+        task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
+        
+        let movie = movies[indexPath.row ]
+        let title = movie["original_title"] as! String
+        let synposis = movie["overview"] as! String
+      
+        cell.titleLabel.text =  title
+        cell.synopsisLabel.text =  synposis
+        
+        let baseUrl = "https://image.tmbd.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        let test = "https://image.tmdb.org/t/p/w185/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg" as! String
+        let testUrl = URL(string: test)
+        
+        cell.posterView.af.setImage(withURL: testUrl!)
+        
+        print(baseUrl + posterPath)
+
+        return cell
+    }
+     
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
